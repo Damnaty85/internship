ce86a64f05cf9fbcfff805cd5daf901f9b6a99f9
Damnaty85 <54323924+Damnaty85@users.noreply.github.com>
2021-12-16 17:57:31 +0300

    iteration-6

---
+++ dist/const.js
@@ -0,0 +1,17 @@
+export const TITLE = ["Death Proof", "Django Unchained", "Kill Bill", "Inglorious Bastard", "Once upon a time in Hollywood"];
+export const DESCRIPTION = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra.`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis.`,
+    `Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`
+];
+export const POSTERS = [
+    'http://cdn.shopify.com/s/files/1/0037/8008/3782/products/Once_Upon_a_Time_in_Hollywood_French_Grande_Cropped_54ee920c-ef59-4b44-9369-661619dce53c-273171.jpg?v=1611688210',
+    'https://plaqat.ru/images/10616.jpg',
+    'https://i.pinimg.com/originals/c1/f1/b3/c1f1b3e0114944178461606dedb47383.jpg',
+    'https://i.pinimg.com/originals/fd/24/63/fd24637188784e983f5500d18f46536e.jpg',
+    'https://m.media-amazon.com/images/I/71t73NDu29L._AC_SL1000_.jpg',
+    'https://cdn.shopify.com/s/files/1/1057/4964/products/once-upon-a-time-in-hollywood-vintage-movie-poster-original-48x72_5eca4e0d-9b47-4729-99cc-0d88104aa639_300x@2x.jpg?v=1587871194',
+    'https://ae01.alicdn.com/kf/H8874655a44cd4f089c16f1400947f96dw/Canvas-painting-Quentin-Tarantino-Movie-Retro-Poster-Fiction-Reservoir-Dog-Inglourious-Basterds-Posters-Wall-Home-Room.jpg_Q90.jpg_.webp'
+]
+export const GENRES = [`Biography`,`Comedy`,`Crime`,`Drama`,`Fantasy`,`History`,`Horror`,`Mystery`,`Sci-Fi`,`Thriller`,`War`];
+export const ACTORS = [`Robert De Niro`,`Leonardo DiCaprio`,`Tom Hanks`,`Anthony Hopkins`,`Liam Neeson`,`Salma Hayek`,`Anna De Armas`,`Keanu Reeves`];
--- dist/script.js
+++ dist/script.js
@@ -1,22 +1,9 @@
-const TITLE = ["Death Proof", "Django Unchained", "Kill Bill", "Inglorious Bastard", "Once upon a time in Hollywood"];
-const DESCRIPTION = [
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra.`,
-    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis.`,
-    `Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`
-];
-const POSTERS = [
-    'http://cdn.shopify.com/s/files/1/0037/8008/3782/products/Once_Upon_a_Time_in_Hollywood_French_Grande_Cropped_54ee920c-ef59-4b44-9369-661619dce53c-273171.jpg?v=1611688210',
-    'https://plaqat.ru/images/10616.jpg',
-    'https://i.pinimg.com/originals/c1/f1/b3/c1f1b3e0114944178461606dedb47383.jpg',
-    'https://i.pinimg.com/originals/fd/24/63/fd24637188784e983f5500d18f46536e.jpg',
-    'https://m.media-amazon.com/images/I/71t73NDu29L._AC_SL1000_.jpg',
-    'https://cdn.shopify.com/s/files/1/1057/4964/products/once-upon-a-time-in-hollywood-vintage-movie-poster-original-48x72_5eca4e0d-9b47-4729-99cc-0d88104aa639_300x@2x.jpg?v=1587871194',
-    'https://ae01.alicdn.com/kf/H8874655a44cd4f089c16f1400947f96dw/Canvas-painting-Quentin-Tarantino-Movie-Retro-Poster-Fiction-Reservoir-Dog-Inglourious-Basterds-Posters-Wall-Home-Room.jpg_Q90.jpg_.webp'
-]
-const GENRES = [`Biography`,`Comedy`,`Crime`,`Drama`,`Fantasy`,`History`,`Horror`,`Mystery`,`Sci-Fi`,`Thriller`,`War`];
-const ACTORS = [`Robert De Niro`,`Leonardo DiCaprio`,`Tom Hanks`,`Anthony Hopkins`,`Liam Neeson`,`Salma Hayek`,`Anna De Armas`,`Keanu Reeves`];
+import {TITLE, POSTERS, DESCRIPTION, GENRES, ACTORS} from "./const.js"
 
 document.addEventListener('DOMContentLoaded', function() {
+    const TOTAL_FILM = 100;
+    const FILM_ON_PAGE = 20;
+    const SHOWING_MORE = 10
 
     const app = document.querySelector('#app');
     const end = document.getElementById('endofscreen')
@@ -43,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     };
 
-    const generateCards = (count = 10) => {
+    const generateCards = (count = FILM_ON_PAGE) => {
         return new Array(count)
             .fill(``)
             .map(generateCard);
@@ -54,25 +41,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const listItemTemplate = function (array) {
             return `
-                <ul>
+                <span>
                     ${array.map((it) => {
-                        return `<li class="film-card__list-item">${it}</li>`
+                        return `${it}`
                     }).join(', ')}
-                </ul>
+                </span>
             `
         }
 
         return (`
             <article class="film-card">
-                <h3 class="film-card__title">${title}</h3>
-                <div class="film-card__info">
-                    ${listItemTemplate(genres)}
-                    ${listItemTemplate(actors)}
+                <div class="film-card__header">
+                    <h3 class="film-card__title">${title}</h3>
+                    ${
+                        genres.length !== 0 ?
+                            `<div class="film-card__info">
+                                <span>Genres: </span>
+                                ${listItemTemplate(genres)}
+                            </div>` : ``
+                    }
                 </div>
-                <div class="film-card__poster">
-                    <img src="${poster}" alt="${title}">
+                <div class="film-card__body">
+                    <div class="film-card__poster">
+                        <img src="${poster}" alt="${title}">
+                    </div>
                 </div>
-                <p class="film-card__description">${description}</p>
+                <div class="film-card__footer">
+                    ${
+                        genres.length !== 0 ?
+                            `<div class="film-card__info">
+                                <span>Actors: </span>
+                                ${listItemTemplate(actors)}
+                            </div>` : ``
+                    }
+                    <p class="film-card__description">${description}</p>
+                </div>
             </article>
         `);
     };
@@ -106,7 +109,7 @@ document.addEventListener('DOMContentLoaded', function() {
         constructor(card) {
             super();
 
-            this.card = card;
+            this._card = card;
         }
 
         get card() {
@@ -130,28 +133,44 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function* pageGenerator(page = 10) {
-        for (let i = 0; i <= page; i++) {
-            yield generateCards();
-        }
+    let showingFilmsCount = FILM_ON_PAGE;
+    let dataFilm = [];
+
+    function renderFilm(film) {
+        const template = new Card(film);
+        app.insertAdjacentHTML('beforeend', template.getTemplate())
     }
 
-    const generator = pageGenerator();
+    function renderFilms(data) {
+        data.slice(0, showingFilmsCount).forEach((film) => {
+            renderFilm(film)
+        });
+    }
+
+    if(localStorage.getItem("films")) {
+        dataFilm = JSON.parse(localStorage.getItem('films'))
+        renderFilms(dataFilm)
+    } else {
+        localStorage.setItem('films', JSON.stringify(generateCards(TOTAL_FILM)))
+        dataFilm = JSON.parse(localStorage.getItem('films'))
+        renderFilms(dataFilm)
+    }
 
     const observer = new IntersectionObserver(entries => {
         const firstEntry = entries[0];
         if (firstEntry.isIntersecting) {
+            const prevTasksCount = showingFilmsCount;
+            showingFilmsCount = showingFilmsCount + SHOWING_MORE;
             try{
-                if(generator.next().done === false) {
-                    generator.next().value.forEach((card) => {
-                        const template = new Card(card);
-                        app.insertAdjacentHTML('beforeend', template.getTemplate())
-                    });
+                if (showingFilmsCount >= dataFilm.length) {
+                    throw new Error('End of list, try again later')
                 } else {
-                    throw new Error("Sorry, this is the end of list")
+                    dataFilm.slice(prevTasksCount, showingFilmsCount).forEach((film) => {
+                        renderFilm(film)
+                    });
                 }
-            }catch(err) {
-                if (err.name === "Error") {
+            } catch(err) {
+                if(err.name === 'Error') {
                     alert(err.message)
                 }
             }
@@ -159,5 +178,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }, {rootMargin: '100px'});
 
     observer.observe(end);
-    
+
 })
+
